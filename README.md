66c8f348fffd5234257b62bfa87c2f628788fcbc
Edomiyas <temesgen.edomiyas@yahoo.com>
2024-12-24 16:54:40 +0100

    ObjLab API setup crée avec mise en place mongodb

---
+++ README.md
@@ -0,0 +1,3 @@
+## ObjLab API
+
+Ceci est l'api pour le projet **ObjLab** lié au test technqiue de TROOV.
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+
+mongoose
+  .connect(process.env.MONGO_URL || "")
+  .then(() => console.log("Connected to MongoDB database !"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(cors());
+
+const url = process.env.API_URL || "http://localhost:3000";
+const port = process.env.API_PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("ObjLab API");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at ${url}:${port}`);
+});
